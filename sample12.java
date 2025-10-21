98ff6593ff40b0729c1e2d0f8282724d961cc091
112360113 <153254992+ntut112360113@users.noreply.github.com>
2025-10-21 20:16:14 +0800

    Update sample12.java

--- sample12.java
+++ sample12.java
@@ -1,5 +1,5 @@
 
-public class simple12
+public class sample12
 {
 	public static void main(String[] args) 
 	{
@@ -21,4 +21,5 @@ public class simple12
 		System.out.println("因為是在指定值之後才遞增,所以b的值為"+b);
         System.out.println("因為是在遞增之後才指定值,所以c的值為"+c);
 	}
+
 }
